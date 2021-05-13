aa6290c92b07a77e4ad9a4dd9298f3eb170f9a1b
leeguo <leeguo2015@163.com>
2021-05-14 00:57:11 +0800

    go版本

--- go_/delete_element.go
+++ go_/delete_element.go
@@ -4,44 +4,33 @@ import "fmt"
 
 func main() {
 	//nums := []int{1, 1, 1, 1, 2, 3, 4, 4, 4}
-	nums := []int{1, 1, 1, 1}
 	//nums := []int{1, 1, 1, 1}
+	//nums := []int{0, 0, 0, 0, 0}
+	//nums := []int{1, 1}
+	nums := []int{1, 2, 2}
 	//nums := []int{1, 2, 3, }
 	var l int
 	l = removeDuplicates(nums)
 	fmt.Println(l)
 }
 
+//思路: 1外层循环i, 找i+1 是否相等, 如果相等, 则删除 ,
+//		数组集体前移, 再次判断i+1
+
 func removeDuplicates(nums []int) int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := 0; j < len(nums)-i; j++ {
+	for i := 0; i < len(nums); i++ {
+		for true {
+			if len(nums) <= i+1 {
+				break
+			}
 			if nums[i] == nums[i+1] {
 				nums = append(nums[:i], nums[i+1:]...)
 			} else {
 				break
 			}
+
 		}
 	}
+
 	return len(nums)
 }
-
-//func removeDuplicates(nums []int) int {
-//	fmt.Println(nums)
-//	for i := 0; i < len(nums)-1; i++ {
-//		for j := 0; j < len(nums)-i; j++ {
-//			fmt.Printf("第一位i下标%v, 值为%v, 子数组j 下标%v, 值为%v \n", i, nums[i], j, nums[i+j])
-//			if nums[i] == nums[i+1] {
-//				fmt.Printf("删除的元素%v ,删除元素下标为%v, 前的数组为: %v\n", nums[i+j], i+j, nums)
-//				nums = append(nums[:i], nums[i+1:]...)
-//				fmt.Printf("后的数组为: %v\n", nums)
-//			} else {
-//				fmt.Printf("不删除的元素%v ,删除元素i下标为%v,  前的数组为: %v 不删除的j元素%v ,删除元素j下标为%v,\n", nums[i], i, nums, nums[i+j], i+j)
-//				break
-//			}
-//		}
-//
-//	}
-//	fmt.Println(nums)
-//
-//	return len(nums)
-//}
