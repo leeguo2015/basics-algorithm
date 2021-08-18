b4cce9f016745a16977b575eab7e2f06c8bb7c23
leeguo <leeguo2015@163.com>
2021-08-18 19:57:07 +0800

    AC 证书相关知识(还未封装好)

---
+++ Neighbor/CA/cert.go
@@ -0,0 +1,251 @@
+package CA
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"math/big"
+	"net"
+	"os"
+	"time"
+)
+
+//ECDSASign 签名返回信息结构
+type ECDSASign struct {
+	R, S *big.Int
+}
+
+/*
+	name:DigestData
+	feature:生成明文的摘要数据,这里获取sha256的哈希值作为摘要数据
+	param:
+		data:明文数据
+	return:
+		err:错误信息
+		digestData:明文的摘要
+	author:ali 2020.9.24
+*/
+func DigestData(data []byte) []byte {
+	hash := sha256.New()
+	hash.Write(data)
+	bytes := hash.Sum(nil)
+	return bytes
+	//return sha256.Sum256(data)
+}
+
+func CreateCer() {
+	max := new(big.Int).Lsh(big.NewInt(1), 128)   //把 1 左移 128 位，返回给 big.Int
+	serialNumber, _ := rand.Int(rand.Reader, max) //返回在 [0, max) 区间均匀随机分布的一个随机值
+	subject := pkix.Name{                         //Name代表一个X.509识别名。只包含识别名的公共属性，额外的属性被忽略。
+		Organization:       []string{"Manning Publications Co."},
+		OrganizationalUnit: []string{"Books"},
+		CommonName:         "Go Web Programming",
+	}
+	template := x509.Certificate{
+		SerialNumber: serialNumber, // SerialNumber 是 CA 颁布的唯一序列号，在此使用一个大随机数来代表它
+		Subject:      subject,
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(10 * 365 * 24 * time.Hour),
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature, //KeyUsage 与 ExtKeyUsage 用来表明该证书是用来做服务器认证的
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},               // 密钥扩展用途的序列
+		IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},
+	}
+	pk, _ := rsa.GenerateKey(rand.Reader, 2048) //生成一对具有指定字位数的RSA密钥
+
+	//CreateCertificate基于模板创建一个新的证书
+	//第二个第三个参数相同，则证书是自签名的
+	//返回的切片是DER编码的证书
+	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &pk.PublicKey, pk) //DER 格式
+	certOut, _ := os.Create("cert.pem")
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICAET", Bytes: derBytes})
+	certOut.Close()
+	keyOut, _ := os.Create("key.pem")
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(pk)})
+	keyOut.Close()
+}
+
+func GetPublic(certBytes []byte) (string, error) {
+	block, _ := pem.Decode(certBytes)
+	if block == nil {
+		return "", errors.New("certBytes not cant decode pem")
+	}
+	certBody, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+	// 提取公钥
+	publicKeyDer, _ := x509.MarshalPKIXPublicKey(certBody.PublicKey)
+	publicKeyBlock := pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyDer,
+	}
+	publicKeyPem := string(pem.EncodeToMemory(&publicKeyBlock))
+	//可以根据证书结构解析
+	//fmt.Println(certBody.SignatureAlgorithm) // 加密算法
+	return publicKeyPem, nil
+
+}
+
+func GetDN(certBytes []byte) (string, error) {
+	block, _ := pem.Decode(certBytes)
+	if block == nil {
+		return "", errors.New("certBytes not cant decode pem")
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+	var subject pkix.RDNSequence
+	if _, err := asn1.Unmarshal(cert.RawSubject, &subject); err != nil {
+		return "", err
+	}
+	return subject.String(), nil
+}
+
+//签名
+func RsaSignWithSha256(data []byte, keyBytes []byte) []byte {
+	//h := sha1.New()
+	h := sha256.New()
+	h.Write(data)
+	hashed := h.Sum(nil)
+	block, _ := pem.Decode(keyBytes)
+	if block == nil {
+		panic(errors.New("private key error"))
+	}
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		fmt.Println("ParsePKCS8PrivateKey err", err)
+		panic(err)
+	}
+
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed)
+	if err != nil {
+		fmt.Printf("Error from signing: %s\n", err)
+		panic(err)
+	}
+
+	return signature
+}
+
+// 私钥解密
+func RsaDecrypt(ciphertext, keyBytes []byte) []byte {
+	//获取私钥
+	block, _ := pem.Decode(keyBytes)
+	if block == nil {
+		panic(errors.New("private key error!"))
+	}
+	//解析PKCS1格式的私钥
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	// 解密
+	data, err := rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+//验证
+func RsaVerySignWithSha256(data, signData, keyBytes []byte) bool {
+	block, _ := pem.Decode(keyBytes)
+	if block == nil {
+		panic(errors.New("public key error"))
+	}
+	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	hashed := sha256.Sum256(data)
+	err = rsa.VerifyPKCS1v15(pubKey.(*rsa.PublicKey), crypto.SHA256, hashed[:], signData)
+	if err != nil {
+		panic(err)
+	}
+	return true
+}
+
+// 公钥加密
+func RsaEncrypt(data, keyBytes []byte) []byte {
+	//解密pem格式的公钥
+	block, _ := pem.Decode(keyBytes)
+	if block == nil {
+		panic(errors.New("public key error"))
+	}
+	// 解析公钥
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	// 类型断言
+	pub := pubInterface.(*rsa.PublicKey)
+	//加密
+	ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader, pub, data)
+	if err != nil {
+		panic(err)
+	}
+	return ciphertext
+}
+
+func SignData(data string, block *pem.Block) (signData string, err error) {
+	//privateKey, err := rsa.Pa(block.Bytes)
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		fmt.Println("ParsePKCS8PrivateKey :", err)
+		fmt.Println("privateKey :", privateKey)
+		return signData, err
+	}
+	fmt.Println("privateKey :", privateKey)
+
+	msg := []byte(data)
+	// Before signing, we need to hash our message
+	// The hash is what we actually sign
+	msgHash := sha256.New()
+	_, err = msgHash.Write(msg)
+	if err != nil {
+		panic(err)
+	}
+	msgHashSum := msgHash.Sum(nil)
+
+	//if ecPrivKey, ok := privateKey.(*rsa.PrivateKey); ok {	}//使用类型断言，否则会panic
+	//digestData := DigestData([]byte(data))
+
+	//result, err := ecdsa.SignASN1(rand.Reader, ecPrivKey, digestData)//SignASN1接口在golang1.15版本才引入的
+	//singDataBytes, err := rsa.SignPSS(rand.Reader, privateKey, digestData)
+	singDataBytes, err := rsa.SignPSS(rand.Reader, privateKey, crypto.SHA256, msgHashSum, nil)
+	if err != nil {
+		return signData, err
+	}
+	fmt.Println(singDataBytes)
+	fmt.Println(string(singDataBytes))
+	//signData = base64.StdEncoding.EncodeToString(result)
+
+	return signData, err
+}
+
+//
+//func DecryptKey(encodeKey, priCertPath string) (key string, err error) {
+//	//1.解密密钥密文
+//	privateKey, err := GetPrivateKey(priCertPath)
+//	if err != nil {
+//		tlog.Error("读取私钥失败：%s", err)
+//		return key, err
+//	}
+//	//2.解密密文
+//	eciesPri := ecies.ImportECDSA(privateKey)
+//	decodeKey, _ := base64.StdEncoding.DecodeString(encodeKey)
+//	dt, err := eciesPri.Decrypt(rand.Reader, decodeKey, nil, nil)
+//	if err != nil {
+//		return key, err
+//	}
+//	key = string(dt)
+//	return key, err
+//}
