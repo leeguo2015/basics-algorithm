36a3bd3892bc99837d8d5ab62fe55facc0e4f9bf
leeguo <leeguo2015@163.com>
2022-12-10 22:55:17 +0800

    [algorithm] [medium] 1780. 判断一个数字是否可以表示成三的幂的和

---
+++ QuestionGo/algorithm/checkPowersOfThree/checkPowersOfThree.go
@@ -0,0 +1,11 @@
+package checkPowersOfThree
+
+func checkPowersOfThree(n int) bool {
+	for n > 0 {
+		if n%3 > 1 {
+			return false
+		}
+		n /= 3
+	}
+	return true
+}
---
+++ QuestionGo/algorithm/checkPowersOfThree/checkPowersOfThree_test.go
@@ -0,0 +1,25 @@
+package checkPowersOfThree
+
+import "testing"
+
+type Detail struct {
+	want  bool
+	mater int
+}
+
+var TestList = []Detail{
+	{want: true, mater: 91},
+	{want: true, mater: 12},
+	{want: false, mater: 21},
+}
+
+func Test_numDifferentIntegers(t *testing.T) {
+
+	for _, detail := range TestList {
+		if got := checkPowersOfThree(detail.mater); detail.want != got {
+			t.Errorf("test failed want:%#v, got %#v, mater: %#v", detail.want, got, detail.mater)
+		}
+	}
+	t.Log("pass all test case")
+
+}
